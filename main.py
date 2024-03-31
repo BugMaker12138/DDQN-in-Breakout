b0cfdd71208491f2e9ac1813ed9a9c10b137aae2
BugMaker12138 <113653874+BugMaker12138@users.noreply.github.com>
2024-03-31 20:00:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ from save_video import save_video
 if __name__ == "__main__":
     # Call the necessary functions based on requirements
     # For example:
+    
+    # train the model 
     train()
-    # max = 0
-    # for i in range(100):
-    #     temp = test()
-    #     if temp > max:
-    #         max = temp
-    # print("Return in the test is :", max)
+    
+    #test the model 
+    max = 0
+    for i in range(100):
+        temp = test()
+        if temp > max:
+            max = temp
+    print("Return in the test is :", max)
+
+    # OPTIONAL!  save the game video  
     # save_video()
+    
     pass
